a3560ba130f45426ca681c4a8e55a13bd1102d77
irudenko <ig.rudenko1@yandex.ru>
2022-11-16 11:59:37 +0300

    v 0.4.25 Можно искать не только по MAC адресу, а и по IP

--- devicemanager/vendors/juniper.py
+++ devicemanager/vendors/juniper.py
@@ -6,13 +6,23 @@ class Juniper(BaseDevice):
     prompt = r"> $"
     space_prompt = r"-+\(more.*?\)-+"
     vendor = "juniper"
-    mac_format = r"\S\S:" * 5 + r"\S\S"
+    mac_format = r"\S\S:\S\S:\S\S:\S\S:\S\S:\S\S"
 
     def search_mac(self, mac_address: str) -> list:
         """Ищем MAC адрес в таблице ARP оборудования"""
 
         formatted_mac = "{}{}:{}{}:{}{}:{}{}:{}{}:{}{}".format(*mac_address)
 
+        # >> Ищем среди subscribers <<
+        subscribers_output = self.send_command(
+            f"show subscribers mac-address {formatted_mac} detail", expect_command=False
+        )
+        formatted_result = self.parse_subscribers(subscribers_output)
+        if formatted_result:
+            # Нашли среди subscribers
+            return formatted_result[0]
+
+        # >> Ищем в таблице ARP <<
         match = self.send_command(
             f"show arp | match {formatted_mac}", expect_command=False
         )
@@ -23,9 +33,9 @@ class Juniper(BaseDevice):
             encoding="utf-8",
         ) as template_file:
             template = textfsm.TextFSM(template_file)
-
         formatted_result = template.ParseText(match)
         if formatted_result:
+            # Нашли в таблице ARP
             return formatted_result[0]
 
         return []
@@ -33,6 +43,16 @@ class Juniper(BaseDevice):
     def search_ip(self, ip_address: str) -> list:
         """Ищем IP адрес в таблице ARP оборудования"""
 
+        # >> Ищем среди subscribers <<
+        subscribers_output = self.send_command(
+            f"show subscribers address {ip_address} detail", expect_command=False
+        )
+        formatted_result = self.parse_subscribers(subscribers_output)
+        if formatted_result:
+            # Нашли среди subscribers
+            return formatted_result[0]
+
+        # >> Ищем в таблице ARP <<
         match = self.send_command(
             f"show arp | match {ip_address}", expect_command=False
         )
@@ -49,6 +69,16 @@ class Juniper(BaseDevice):
             return formatted_result[0]
 
         return []
+
+    def parse_subscribers(self, string: str) -> list:
+        # Форматируем вывод
+        with open(
+            f"{TEMPLATE_FOLDER}/{self.vendor.lower()}-{self.model.lower()}/subscribers.template",
+            encoding="utf-8",
+        ) as template_file:
+            template = textfsm.TextFSM(template_file)
+
+        return template.ParseText(string)
 
     def get_interfaces(self) -> list:
         pass
