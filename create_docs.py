5815340ac1e1baadd1bb40131a46fde715c416d0
irudenko <ig.rudenko1@yandex.ru>
2022-11-18 16:32:21 +0300

    v 0.5 Документирование кода

---
+++ create_docs.py
@@ -0,0 +1,22 @@
+import django
+import pdoc
+import os
+import pathlib
+
+
+if __name__ == "__main__":
+    os.environ["DJANGO_SETTINGS_MODULE"] = "ecstasy_project.settings"
+    django.setup()
+
+    doc = pdoc.pdoc(
+        "app_settings",
+        "!app_settings.migration",
+        "check",
+        "!check.migration",
+        "!check.tests",
+        "devicemanager",
+        "!devicemanager.tests",
+        "net_tools",
+        "!net_tools.migration",
+        output_directory=pathlib.Path(__file__).parent / "docs",
+    )
