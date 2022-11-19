f741fa2521e582392688fdedfaaabf255b4015bc
ig.rudenko <ig.rudenko@users.noreply.github.com>
2022-11-19 23:36:42 +0300

    v 0.5 Документирование кода + Q-Tech

--- devicemanager/vendors/qtech.py
+++ devicemanager/vendors/qtech.py
@@ -2,12 +2,12 @@ import re
 from time import sleep
 from functools import lru_cache
 import textfsm
-from .base import BaseDevice, TEMPLATE_FOLDER
+from .base import BaseDevice, TEMPLATE_FOLDER, InterfaceList, InterfaceVLANList, MACList
 
 
 class Qtech(BaseDevice):
     """
-    Для оборудования от производителя Q-Tech
+    # Для оборудования от производителя Q-Tech
 
     Проверено для:
      - QSW-8200
@@ -18,7 +18,17 @@ class Qtech(BaseDevice):
     mac_format = r"\S\S-" * 5 + r"\S\S"
     vendor = "Q-Tech"
 
-    def get_interfaces(self) -> list:
+    def get_interfaces(self) -> InterfaceList:
+        """
+        ## Возвращаем список всех интерфейсов на устройстве
+
+        Команда на оборудовании:
+
+            # show interface ethernet status
+
+        :return: ```[ ('name', 'status', 'desc'), ... ]```
+        """
+
         output = self.send_command(
             command="show interface ethernet status", expect_command=False
         )
@@ -29,11 +39,28 @@ class Qtech(BaseDevice):
             int_des_ = textfsm.TextFSM(template_file)
             result = int_des_.ParseText(output)  # Ищем интерфейсы
         return [
-            [line[0], line[1].lower().replace("a-", "admin "), line[2]]
+            (line[0], line[1].lower().replace("a-", "admin "), line[2])
             for line in result
         ]
 
-    def get_vlans(self) -> list:
+    def get_vlans(self) -> InterfaceVLANList:
+        """
+        ## Возвращаем список всех интерфейсов и его VLAN на коммутаторе.
+
+        Для начала получаем список всех интерфейсов через метод **get_interfaces()**
+
+        Затем для каждого интерфейса смотрим конфигурацию
+
+            # show running-config interface ethernet {port}
+
+        И выбираем строчки, в которых указаны VLAN:
+
+         - ```vlan {vid},{vid},...{vid}```
+         - ```vlan add {vid},{vid},...{vid}```
+
+        :return: ```[ ('name', 'status', 'desc', ['{vid}', '{vid},{vid},...{vid}', ...] ), ... ]```
+        """
+
         result = []
         for line in self.get_interfaces():
             if not line[0].startswith("V"):
@@ -46,12 +73,25 @@ class Qtech(BaseDevice):
                 vlans = []
                 for v in vlans_group:
                     vlans += v.split(";")
-                result.append(line + [vlans])
+                result.append((line[0], line[1], line[2], vlans))
 
         return result
 
     @staticmethod
     def validate_port(port: str) -> (str, None):
+        """
+        Проверяем порт на валидность
+
+        >>> Qtech.validate_port("1/2/1")
+        '1/2/1'
+        >>> Qtech.validate_port("1/1/21")
+        '1/1/21'
+        >>> Qtech.validate_port("23")
+        None
+        >>> Qtech.validate_port("port12")
+        None
+        """
+
         port = port.strip()
         if bool(re.findall(r"^\d+/\d+/\d+$", port)):
             return port
@@ -59,9 +99,16 @@ class Qtech(BaseDevice):
 
     def get_mac(self, port: str) -> list:
         """
-        Поиск маков на порту
-        :return: [ ('vid', 'mac'), ... ]
+        ## Возвращаем список из VLAN и MAC-адреса для данного порта.
+
+        Команда на оборудовании:
+
+            # show mac-address-table interface ethernet {port}
+
+        :param port: Номер порта коммутатора
+        :return: ```[ ('vid', 'mac'), ... ]```
         """
+
         port = self.validate_port(port)
         if port is None:
             return []
@@ -71,6 +118,30 @@ class Qtech(BaseDevice):
         return macs
 
     def reload_port(self, port: str, save_config=True) -> str:
+        """
+        ## Перезагружает порт
+
+        Переходим в режим конфигурирования:
+
+            # config terminal
+
+        Переходим к интерфейсу:
+
+            (config)# interface ethernet {port}
+
+        Перезагружаем порт:
+
+            (config-if)# shutdown
+            (config-if)# no shutdown
+
+        Выходим из режима конфигурирования:
+
+            (config-if)# end
+
+        :param port: Порт для перезагрузки
+        :param save_config: Если True, конфигурация будет сохранена на устройстве, defaults to True (optional)
+        """
+
         port = self.validate_port(port)
         if port is None:
             return f"Неверный порт {port}"
@@ -90,6 +161,26 @@ class Qtech(BaseDevice):
         return r + s
 
     def set_port(self, port, status, save_config=True):
+        """
+        ## Устанавливает статус порта на коммутаторе **up** или **down**
+
+        Переходим в режим конфигурирования:
+            # config terminal
+
+        Переходим к интерфейсу:
+            (config)# interface ethernet {port}
+
+        Меняем состояние порта:
+            (config-if)# {shutdown|no shutdown}
+
+        Выходим из режима конфигурирования:
+            (config-if)# end
+
+        :param port: Порт
+        :param status: "up" или "down"
+        :param save_config: Если True, конфигурация будет сохранена на устройстве, defaults to True (optional)
+        """
+
         port = self.validate_port(port)
         if port is None:
             return f"Неверный порт {port}"
@@ -110,6 +201,16 @@ class Qtech(BaseDevice):
         return s
 
     def save_config(self):
+        """
+        ## Сохраняем конфигурацию оборудования командой:
+
+            # write
+            Y
+
+        Ожидаем ответа от оборудования **successful**,
+        если нет, то ошибка сохранения
+        """
+
         self.session.sendline("write")
         self.session.sendline("Y")
         if self.session.expect([self.prompt, "successful"]):
@@ -124,7 +225,17 @@ class Qtech(BaseDevice):
         return f"<p>{port_type}</p>"
 
     def get_port_info(self, port):
-        """Общая информация о порте"""
+        """
+        ## Возвращаем информацию о порте.
+
+        Через команду:
+
+            # show interface ethernet{port}
+
+        :param port: Номер порта, для которого требуется получить информацию
+        :return: Информация о порте или ```"Неверный порт {port}"```
+        """
+
         port = self.validate_port(port)
         if port is None:
             return f"Неверный порт {port}"
@@ -132,7 +243,12 @@ class Qtech(BaseDevice):
         return "<br>".join(self.__get_port_info(port).split("\n")[:10])
 
     def port_type(self, port):
-        """Определяем тип порта: медь или оптика"""
+        """
+        ## Возвращает тип порта
+
+        :param port: Порт для проверки
+        :return: "SFP", "COPPER" или "Неверный порт {port}"
+        """
 
         port = self.validate_port(port)
         if port is None:
@@ -145,6 +261,11 @@ class Qtech(BaseDevice):
         return "COPPER"
 
     def get_port_errors(self, port):
+        """
+        ## Выводим ошибки на порту
+
+        :param port: Порт для проверки на наличие ошибок
+        """
 
         port = self.validate_port(port)
         if port is None:
@@ -158,7 +279,13 @@ class Qtech(BaseDevice):
         return "\n".join(result)
 
     def port_config(self, port):
-        """Конфигурация порта"""
+        """
+        ## Выводим конфигурацию порта
+
+        Используем команду:
+
+            # show running-config interface ethernet {port}
+        """
 
         port = self.validate_port(port)
         if port is None:
@@ -167,6 +294,36 @@ class Qtech(BaseDevice):
         return self.send_command(f"show running-config interface ethernet {port}")
 
     def set_description(self, port: str, desc: str) -> str:
+        """
+        ## Устанавливаем описание для порта предварительно очистив его от лишних символов
+
+        Если длина описания больше допустимой, то вернется ```"Max length:{max_length}"```
+
+        Переходим в режим конфигурирования:
+
+            # config terminal
+
+        Переходим к интерфейсу:
+
+            (config)# interface ethernet {port}
+
+        Если была передана пустая строка для описания, то очищаем с помощью команды:
+
+            (config-if)# no description
+
+        Если **desc** содержит описание, то используем команду для изменения:
+
+            (config-if)# description {desc}
+
+        Выходим из режима конфигурирования:
+
+            (config-if)# end
+
+        :param port: Порт, для которого вы хотите установить описание
+        :param desc: Описание, которое вы хотите установить для порта
+        :return: Вывод команды смены описания
+        """
+
         port = self.validate_port(port)
         if port is None:
             return f"Неверный порт {port}"
