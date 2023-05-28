b14e5e25689e3f2c3866f96e081073e56237cec0
ig.rudenko <ig.rudenko@users.noreply.github.com>
2023-05-28 13:00:12 +0300

    v 0.13.11 refactoring tests for mysql database

--- check/tests/test_models.py
+++ check/tests/test_models.py
@@ -10,89 +10,89 @@ class DeviceModelTest(TestCase):
 
     # NAME
     def test_device_name_label(self):
-        dev = Devices.objects.get(id=1)
+        dev = Devices.objects.all().first()
         name_label = dev._meta.get_field("name").verbose_name
         self.assertEqual(name_label, "Имя оборудования")
 
     def test_device_name_max_length(self):
-        dev = Devices.objects.get(id=1)
+        dev = Devices.objects.all().first()
         max_length = dev._meta.get_field("name").max_length
         self.assertEqual(max_length, 100)
 
     # IP
     def test_ip_label(self):
-        dev = Devices.objects.get(id=1)
+        dev = Devices.objects.all().first()
         ip_label = dev._meta.get_field("ip").verbose_name
         self.assertEqual(ip_label, "IP адрес")
 
     def test_ip_max_length(self):
-        dev = Devices.objects.get(id=1)
+        dev = Devices.objects.all().first()
         max_length = dev._meta.get_field("ip").max_length
         self.assertEqual(max_length, 39)
 
     # VENDOR
     def test_vendor_label(self):
-        dev = Devices.objects.get(id=1)
+        dev = Devices.objects.all().first()
         vendor_label = dev._meta.get_field("vendor").verbose_name
         self.assertEqual(vendor_label, "Производитель")
 
     def test_vendor_max_length(self):
-        dev = Devices.objects.get(id=1)
+        dev = Devices.objects.all().first()
         max_length = dev._meta.get_field("vendor").max_length
         self.assertEqual(max_length, 100)
 
     # SNMP_COMMUNITY
     def test_snmp_comm_label(self):
-        dev = Devices.objects.get(id=1)
+        dev = Devices.objects.all().first()
         label = dev._meta.get_field("snmp_community").verbose_name
         self.assertEqual(label, "SNMP community")
 
     def test_snmp_comm_max_length(self):
-        dev = Devices.objects.get(id=1)
+        dev = Devices.objects.all().first()
         max_length = dev._meta.get_field("snmp_community").max_length
         self.assertEqual(max_length, 64)
 
     def test_default_snmp_comm(self):
-        dev = Devices.objects.get(id=1)
+        dev = Devices.objects.all().first()
         self.assertEqual(dev.snmp_community, None)
 
     # port_scan_protocol
     def test_port_scan_protocol_label(self):
-        dev = Devices.objects.get(id=1)
+        dev = Devices.objects.all().first()
         label = dev._meta.get_field("port_scan_protocol").verbose_name
         self.assertEqual(label, "Протокол для поиска интерфейсов")
 
     def test_port_scan_protocol_max_length(self):
-        dev = Devices.objects.get(id=1)
+        dev = Devices.objects.all().first()
         max_length = dev._meta.get_field("port_scan_protocol").max_length
         self.assertEqual(max_length, 6)
 
     def test_default_port_scan_protocol(self):
-        dev = Devices.objects.get(id=1)
+        dev = Devices.objects.all().first()
         self.assertEqual(dev.port_scan_protocol, "telnet")
 
     # cmd_protocol
     def test_cmd_protocol_label(self):
-        dev = Devices.objects.get(id=1)
+        dev = Devices.objects.all().first()
         label = dev._meta.get_field("cmd_protocol").verbose_name
         self.assertEqual(label, "Протокол для выполнения команд")
 
     def test_cmd_protocol_max_length(self):
-        dev = Devices.objects.get(id=1)
+        dev = Devices.objects.all().first()
         max_length = dev._meta.get_field("cmd_protocol").max_length
         self.assertEqual(max_length, 6)
 
     def test_default_cmd_protocol(self):
-        dev = Devices.objects.get(id=1)
+        dev = Devices.objects.all().first()
         self.assertEqual(dev.port_scan_protocol, "telnet")
 
     # STR
     def test_object_to_str(self):
-        dev = Devices.objects.get(id=1)
+        dev = Devices.objects.all().first()
         self.assertEqual(f"{dev.name} ({dev.ip})", str(dev))
 
     def test_absolute_url(self):
-        dev = Devices.objects.get(id=1)
+        dev = Devices.objects.all().first()
         self.assertEqual(dev.get_absolute_url(), "/device/" + dev.name)
 
 
@@ -103,29 +103,29 @@ class DeviceGroupTest(TestCase):
 
     # NAME
     def test_name_label(self):
-        g = DeviceGroup.objects.get(id=1)
+        g = DeviceGroup.objects.all().first()
         label = g._meta.get_field("name").verbose_name
         self.assertEqual(label, "Название")
 
     def test_name_max_length(self):
-        g = DeviceGroup.objects.get(id=1)
+        g = DeviceGroup.objects.all().first()
         max_length = g._meta.get_field("name").max_length
         self.assertEqual(max_length, 100)
 
     # DESCRIPTION
     def test_description_label(self):
-        g = DeviceGroup.objects.get(id=1)
+        g = DeviceGroup.objects.all().first()
         label = g._meta.get_field("description").verbose_name
         self.assertEqual(label, "Описание")
 
     def test_description_max_length(self):
-        g = DeviceGroup.objects.get(id=1)
+        g = DeviceGroup.objects.all().first()
         max_length = g._meta.get_field("description").max_length
         self.assertEqual(max_length, 255)
 
     # STR
     def test_object_to_str(self):
-        g = DeviceGroup.objects.get(id=1)
+        g = DeviceGroup.objects.all().first()
         self.assertEqual(f"[ {g.name} ]", str(g))
 
 
@@ -142,62 +142,62 @@ class AuthGroupTest(TestCase):
 
     # NAME
     def test_name_label(self):
-        g = AuthGroup.objects.get(id=1)
+        g = AuthGroup.objects.all().first()
         label = g._meta.get_field("name").verbose_name
         self.assertEqual(label, "Название")
 
     def test_name_max_length(self):
-        g = AuthGroup.objects.get(id=1)
+        g = AuthGroup.objects.all().first()
         max_length = g._meta.get_field("name").max_length
         self.assertEqual(max_length, 100)
 
     # LOGIN
     def test_login_label(self):
-        g = AuthGroup.objects.get(id=1)
+        g = AuthGroup.objects.all().first()
         label = g._meta.get_field("login").verbose_name
         self.assertEqual(label, "Логин")
 
     def test_login_max_length(self):
-        g = AuthGroup.objects.get(id=1)
+        g = AuthGroup.objects.all().first()
         max_length = g._meta.get_field("login").max_length
         self.assertEqual(max_length, 64)
 
     # PASSWORD
     def test_password_label(self):
-        g = AuthGroup.objects.get(id=1)
+        g = AuthGroup.objects.all().first()
         label = g._meta.get_field("password").verbose_name
         self.assertEqual(label, "Пароль")
 
     def test_password_max_length(self):
-        g = AuthGroup.objects.get(id=1)
+        g = AuthGroup.objects.all().first()
         max_length = g._meta.get_field("password").max_length
         self.assertEqual(max_length, 64)
 
     # PASSWORD
     def test_secret_label(self):
-        g = AuthGroup.objects.get(id=1)
+        g = AuthGroup.objects.all().first()
         label = g._meta.get_field("secret").verbose_name
         self.assertEqual(label, "Пароль от привилегированного режима")
 
     def test_secret_max_length(self):
-        g = AuthGroup.objects.get(id=1)
+        g = AuthGroup.objects.all().first()
         max_length = g._meta.get_field("secret").max_length
         self.assertEqual(max_length, 64)
 
     # DESCRIPTION
     def test_description_label(self):
-        g = AuthGroup.objects.get(id=1)
+        g = AuthGroup.objects.all().first()
         label = g._meta.get_field("description").verbose_name
         self.assertEqual(label, "Описание")
 
     def test_description_max_length(self):
-        g = AuthGroup.objects.get(id=1)
+        g = AuthGroup.objects.all().first()
         max_length = g._meta.get_field("description").max_length
         self.assertEqual(max_length, 255)
 
     # STR
     def test_object_to_str(self):
-        g = AuthGroup.objects.get(id=1)
+        g = AuthGroup.objects.all().first()
         self.assertEqual(f"< {g.name} >", str(g))
 
 
@@ -212,9 +212,7 @@ class DeviceRelationsTest(TestCase):
             secret="secret",
             description="Description",
         )
-        Devices.objects.create(
-            name="Test Name", ip="192.168.123.123", group=group, auth_group=auth
-        )
+        Devices.objects.create(name="Test Name", ip="192.168.123.123", group=group, auth_group=auth)
 
     def test_relation(self):
         dev = Devices.objects.get(ip="192.168.123.123")
@@ -227,9 +225,7 @@ class DeviceRelationsTest(TestCase):
 class BrasTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        Bras.objects.create(
-            name="BRAS", ip="192.168.1.1", login="login", password="password"
-        )
+        Bras.objects.create(name="BRAS", ip="192.168.1.1", login="login", password="password")
 
     # NAME
     def test_name_label(self):
@@ -295,9 +291,7 @@ class BrasTest(TestCase):
 class ProfileTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        u = User(
-            first_name="First_Name", email="email@email.com", username="first_user"
-        )
+        u = User(first_name="First_Name", email="email@email.com", username="first_user")
         u.set_password("user_password")
         u.save()
 
@@ -306,20 +300,20 @@ class ProfileTest(TestCase):
         self.assertEqual(p.user.username, "first_user")
 
     def test_default_permissions(self):
-        p = Profile.objects.get(user_id=1)
+        p = Profile.objects.get(user=User.objects.get(username="first_user"))
         self.assertEqual(p.permissions, "read")
 
     def test_permissions_list(self):
-        p = Profile.objects.get(user_id=1)
+        p = Profile.objects.get(user=User.objects.get(username="first_user"))
         self.assertEqual(p.permissions_level, ["read", "reboot", "up_down", "bras"])
 
     def test_permissions_max_length(self):
-        p = Profile.objects.get(user_id=1)
+        p = Profile.objects.get(user=User.objects.get(username="first_user"))
         max_length = p._meta.get_field("permissions").max_length
         self.assertEqual(max_length, 15)
 
     def test_str(self):
-        p = Profile.objects.get(user_id=1)
+        p = Profile.objects.get(user=User.objects.get(username="first_user"))
         self.assertEqual(str(p), f"Profile: {p.user.username}")
 
 
