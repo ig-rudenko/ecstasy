7e6a12d898f9444095991f3a6b30ee4cfe024c8b
irudenko <ig.rudenko1@yandex.ru>
2023-08-16 09:31:27 +0300

    v 16.1.4 bugfix: collect staticfiles
    Signed-off-by: irudenko <ig.rudenko1@yandex.ru>

--- ecstasy_project/settings.py
+++ ecstasy_project/settings.py
@@ -155,17 +155,18 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
-# STATIC_ROOT = BASE_DIR / "static"
+
+if DEBUG:
+    STATICFILES_DIRS = [BASE_DIR / "static"]
+else:
+    STATIC_ROOT = BASE_DIR / "static"
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
