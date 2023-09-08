bd2209fac395cb77390830785ee9f6e639191d72
irudenko <ig.rudenko1@yandex.ru>
2023-09-08 16:21:24 +0300

    v 17 Создание формы добавления технических данных GPON подключения
    Signed-off-by: irudenko <ig.rudenko1@yandex.ru>

--- src/gpon_base/Create_Tech_data.vue
+++ src/gpon_base/Create_Tech_data.vue
@@ -5,9 +5,6 @@
       <h2>Добавление технических данных</h2>
     </div>
 
-    {{ formData }}
-
-
     <div class="plate py-4 w-75 container">
 
       <StepMenu
@@ -28,6 +25,7 @@
 
             <div class="shadow">
               <Dropdown v-model="formData.oltState.deviceName" :options="devicesList" filter
+                        :class="formState.firstStep.deviceName.valid?[]:['p-invalid']"
                         optionLabel="name" placeholder="Выберите устройство">
                 <template #value="slotProps">
                   <div v-if="slotProps.value" class="flex align-items-center">
@@ -54,6 +52,7 @@
 
             <div class="shadow">
               <Dropdown v-model="formData.oltState.devicePort" :options="devicePortList" filter
+                        :class="formState.firstStep.devicePort.valid?[]:['p-invalid']"
                         optionLabel="name" placeholder="Выберите порт">
                 <template #value="slotProps">
                   <div v-if="slotProps.value" class="flex align-items-center">
@@ -94,24 +93,27 @@
       <div v-else-if="current_step===2" class="p-4">
 
         <div class="w-100">
-          <div v-if="!show_new_address_form && !formData.houseB.new_address_valid">
+          <div v-if="!show_new_address_form && !formState.secondStep.new_address.valid">
             <h6 class="px-2">Выберите существующий адрес дома
               <Asterisk/>
             </h6>
             <div class="shadow">
               <Dropdown v-model="formData.houseB.address" :options="addressesList" filter
-                        optionLabel="fullAddress" placeholder="Выберите устройство" class="w-100">
+                        :class="formState.secondStep.address.valid?[]:['p-invalid']"
+                        :optionLabel="getFullAddress" placeholder="Выберите" class="w-100">
                 <template #value="slotProps">
-                  <div v-if="slotProps.value" class="flex align-items-center">
-                    <div>{{ slotProps.value.fullAddress }}</div>
+                  <div v-if="slotProps.value" class="flex align-items-center d-flex">
+                    <BuildingIcon :type="slotProps.value.building_type" width="24" height="24"></BuildingIcon>
+                    <div>{{ getFullAddress(slotProps.value) }}</div>
                   </div>
                   <span v-else>
-                            {{ slotProps.placeholder }}
-                        </span>
+                      {{ slotProps.placeholder }}
+                  </span>
                 </template>
                 <template #option="slotProps">
-                  <div class="flex align-items-center">
-                    <div>{{ slotProps.option.fullAddress }}</div>
+                  <div class="flex align-items-center d-flex">
+                    <BuildingIcon :type="slotProps.option.building_type" width="24" height="24"></BuildingIcon>
+                    <div>{{ getFullAddress(slotProps.option) }}</div>
                   </div>
                 </template>
               </Dropdown>
@@ -128,15 +130,9 @@
             <AddressForm @valid="validNewAddress" :address="formData.houseB.new_address"></AddressForm>
           </Dialog>
 
-          <div v-if="!show_new_address_form && formData.houseB.new_address_valid">
+          <div v-if="!show_new_address_form && formState.secondStep.new_address.valid">
             <h6>
-              регион: {{ this.formData.houseB.new_address.region }}, нас. пункт:
-              {{ this.formData.houseB.new_address.settlement }},
-              {{ this.formData.houseB.new_address.street }} д. {{ this.formData.houseB.new_address.house }}
-              <template v-if="this.formData.houseB.new_address.block">
-                корпус: {{ this.formData.houseB.new_address.block }}
-              </template>
-
+              {{ getFullAddress(formData.houseB.new_address) }}
               <br>
               <template v-if="this.formData.houseB.new_address.apartment_building === 'true'">
                 Многоквартирный дом. Количество этажей: {{ this.formData.houseB.new_address.floors }} /
@@ -164,6 +160,48 @@
           </div>
 
         </div>
+      </div>
+
+      <!-- THIRD STEP -->
+      <div v-else-if="current_step===3" class="p-4">
+
+        <div class="w-100 d-flex">
+          <div v-if="this.formData.houseB.buildType()==='building'" class="py-3 me-4">
+            <div class="flex align-items-center py-1">
+              <RadioButton v-model="formData.end3.type" inputId="splitter" value="splitter"/>
+              <label for="splitter" class="ml-2"><span class="m-2">Сплиттер</span></label>
+            </div>
+            <div class="flex align-items-center">
+              <RadioButton v-model="formData.end3.type" inputId="rizer" value="rizer"/>
+              <label for="rizer" class="ml-2"><span class="m-2">Райзер</span></label>
+            </div>
+          </div>
+
+          <div v-if="formData.end3.type==='splitter'">
+            <div>
+              <h6>Количество портов на сплиттере
+                <Asterisk/>
+              </h6>
+              <Dropdown v-model="formData.end3.splitter.portCount" :options="[4, 8, 16]" class="w-full md:w-14rem"/>
+            </div>
+          </div>
+
+          <div v-if="formData.end3.type==='rizer'">
+            <div>
+              <h6>Количество волокон на райзере
+                <Asterisk/>
+              </h6>
+              <Dropdown v-model="formData.end3.splitter.portCount" :options="[4, 8, 12, 16, 24]"
+                        class="w-full md:w-14rem"/>
+            </div>
+
+
+          </div>
+
+        </div>
+        <RizerFiberColorExample :count="formData.end3.splitter.portCount"/>
+
+
       </div>
 
       <!-- Кнопки -->
@@ -214,29 +252,37 @@
 
     </div>
 
+    {{ formData }}
+
   </div>
 </template>
 
 <script>
-import StepMenu from "./components/StepMenu.vue";
+import StepMenu from "./components/StepMenu.vue"
 import Dropdown from "primevue/dropdown/Dropdown.vue"
 import InputText from "primevue/inputtext/InputText.vue"
 import Textarea from "primevue/textarea/Textarea.vue"
 import Button from "primevue/button/Button.vue"
 import Dialog from "primevue/dialog/Dialog.vue"
+import RadioButton from "primevue/radiobutton/RadioButton.vue"
 
-import AddressForm from "./components/AddressForm.vue";
+import AddressForm from "./components/AddressForm.vue"
 import Asterisk from "./components/Asterisk.vue"
+import BuildingIcon from "./components/BuildingIcon.vue"
+import RizerFiberColorExample from "./components/RizerFiberColorExample.vue";
 
 export default {
   name: "Gpon_base.vue",
   components: {
+    BuildingIcon,
     AddressForm,
     Asterisk,
     Button,
     Dialog,
     Dropdown,
     InputText,
+    RadioButton,
+    RizerFiberColorExample,
     StepMenu,
     Textarea,
   },
@@ -244,6 +290,23 @@ export default {
     return {
       current_step: 1,
       show_new_address_form: false,
+      formState: {
+        firstStep: {
+          deviceName: {valid: true},
+          devicePort: {valid: true},
+          isValid() {
+            return this.devicePort.valid && this.deviceName.valid
+          }
+        },
+        secondStep: {
+          address: {valid: true},
+          new_address: {valid: false},
+          isValid() {
+            return this.address.valid || this.new_address.valid
+          }
+        },
+      },
+
       formData: {
         oltState: {
           deviceName: "",
@@ -262,12 +325,25 @@ export default {
             street: "",
             house: "",
             block: null,
-            apartment_building: 'true',
+            building_type: "building",
             floors: 1,
             total_entrances: 1
           },
-          new_address_valid: false,
+          buildType() {
+            if (this.address) {
+              return this.address.building_type
+            } else {
+              return this.new_address.building_type
+            }
+          },
         },
+        end3: {
+          type: "splitter",
+          splitter: {
+            portCount: 8
+          }
+        }
+
       }
     }
   },
@@ -293,25 +369,93 @@ export default {
 
     addressesList() {
       return [
-        {id: 1, fullAddress: "Севастополь, ул. Колобова 22/10"},
-        {id: 2, fullAddress: "Севастополь, ул. Колобова 23/10"},
-        {id: 3, fullAddress: "Севастополь, ул. Колобова 24/10"},
-        {id: 4, fullAddress: "Севастополь, ул. Колобова 25/10"},
-        {id: 5, fullAddress: "Севастополь, ул. Колобова 26/10"},
-        {id: 6, fullAddress: "Севастополь, ул. Колобова 27/10"},
+        {
+          id: 1,
+          region: "Севастополь",
+          settlement: "Сахарная головка",
+          planStructure: "",
+          street: "улица Тракторная",
+          house: "2",
+          block: null,
+          building_type: 'house',
+          floors: 1,
+          total_entrances: 1
+        },
+        {
+          id: 2,
+          region: "Севастополь",
+          settlement: "Севастополь",
+          planStructure: "",
+          street: "улица Колобова",
+          house: "22",
+          block: null,
+          building_type: 'building',
+          floors: 1,
+          total_entrances: 1
+        },
+        {
+          id: 3,
+          region: "Севастополь",
+          settlement: "Севастополь",
+          planStructure: "",
+          street: "проспект Генерала Острякова",
+          house: "222а",
+          block: 2,
+          building_type: 'building',
+          floors: 1,
+          total_entrances: 1
+        },
+        {
+          id: 4,
+          region: "Севастополь",
+          settlement: "Севастополь",
+          planStructure: "Рыбак-7",
+          street: "",
+          house: "123",
+          block: null,
+          building_type: 'house',
+          floors: 1,
+          total_entrances: 1
+        },
       ]
     }
   },
   methods: {
+
+    stepIsValid() {
+      if (this.current_step === 1) {
+        this.formState.firstStep.deviceName.valid = this.formData.oltState.deviceName.length > 0
+        this.formState.firstStep.devicePort.valid = this.formData.oltState.devicePort.length > 0
+        return this.formState.firstStep.isValid()
+
+      } else if (this.current_step === 2) {
+        this.formState.secondStep.address.valid = Boolean(this.formData.houseB.address)
+        return this.formState.secondStep.isValid()
+      }
+    },
+
     nextStep() {
-      if (this.current_step < 4) this.current_step++
+      if (this.current_step < 4 && this.stepIsValid()) this.current_step++
     },
     prevStep() {
       if (this.current_step > 1) this.current_step--
     },
+
+    getFullAddress(address) {
+      let str = ""
+      if (address.region !== "Севастополь") str += ` ${address.region},`;
+      if (address.settlement !== "Севастополь") str += ` ${address.settlement},`;
+      if (address.planStructure.length) str += `СНТ ${address.planStructure},`;
+      if (address.street.length) str += ` ${address.street},`;
+      str += ` д. ${address.house}`;
+      if (address.block) str += `/${address.block}`;
+      return str
+    },
+
     validNewAddress() {
       this.show_new_address_form = false
-      this.formData.houseB.new_address_valid = true
+      this.formState.secondStep.new_address.valid = true
+      this.formData.houseB.address = null
     },
   },
 }
--- src/gpon_base/components/AddressForm.vue
+++ src/gpon_base/components/AddressForm.vue
@@ -4,18 +4,7 @@
     <div class="d-flex justify-content-between">
       <div class="p-3">
 
-        <svg v-if="address.apartment_building === 'true'"
-             xmlns="http://www.w3.org/2000/svg" width="128" height="128" fill="currentColor" class="me-2"
-             viewBox="0 0 16 16">
-          <path
-              d="M15 .5a.5.5 0 0 0-.724-.447l-8 4A.5.5 0 0 0 6 4.5v3.14L.342 9.526A.5.5 0 0 0 0 10v5.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V14h1v1.5a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5V.5ZM2 11h1v1H2v-1Zm2 0h1v1H4v-1Zm-1 2v1H2v-1h1Zm1 0h1v1H4v-1Zm9-10v1h-1V3h1ZM8 5h1v1H8V5Zm1 2v1H8V7h1ZM8 9h1v1H8V9Zm2 0h1v1h-1V9Zm-1 2v1H8v-1h1Zm1 0h1v1h-1v-1Zm3-2v1h-1V9h1Zm-1 2h1v1h-1v-1Zm-2-4h1v1h-1V7Zm3 0v1h-1V7h1Zm-2-2v1h-1V5h1Zm1 0h1v1h-1V5Z"/>
-        </svg>
-        <svg v-else xmlns="http://www.w3.org/2000/svg" width="128" height="128" fill="currentColor" class="me-2"
-             viewBox="0 0 16 16">
-          <path
-              d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5Z"/>
-          <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6Z"/>
-        </svg>
+        <BuildingIcon :type="address.building_type" width="128" height="128"/>
 
       </div>
 
@@ -36,10 +25,9 @@
       </div>
     </div>
 
-
     <div class="w-100">
       <h6 class="px-2">СНТ/ТСН</h6>
-      <InputText v-model.trim="address.planStructure" class="w-100" type="text"
+      <InputText v-model.trim="address.planStructure" :class="planStructureClasses" type="text"
                  placeholder="Рыбак-7/Сатурн-2"/>
     </div>
 
@@ -55,12 +43,12 @@
 
     <div class="py-3">
       <div class="flex align-items-center py-1">
-        <RadioButton v-model="address.apartment_building" inputId="apartment_building" name="pizza" value="true"/>
-        <label for="apartment_building" class="ml-2"><span class="m-2">Многоквартирный дом</span></label>
+        <RadioButton v-model="address.building_type" inputId="building" value="building"/>
+        <label for="building" class="ml-2"><span class="m-2">Многоквартирный дом</span></label>
       </div>
       <div class="flex align-items-center">
-        <RadioButton v-model="address.apartment_building" inputId="private_house" name="pizza" value="false"/>
-        <label for="private_house" class="ml-2"><span class="m-2">Частный дом</span></label>
+        <RadioButton v-model="address.building_type" inputId="house" value="house"/>
+        <label for="house" class="ml-2"><span class="m-2">Частный дом</span></label>
       </div>
     </div>
 
@@ -78,7 +66,7 @@
         <InputNumber :input-style="{width: '75px'}" :min="1" :max="200" v-model.number="address.block"/>
       </div>
 
-      <template v-if="address.apartment_building === 'true'">
+      <template v-if="address.building_type === 'building'">
         <div class="me-3">
           <h6 class="px-2">Этажность</h6>
           <InputNumber :input-style="{width: '100px'}" :min="1" :max="200" v-model.number="address.floors"/>
@@ -108,11 +96,13 @@ import Button from "primevue/button/Button.vue"
 import RadioButton from "primevue/radiobutton/RadioButton.vue"
 
 import Asterisk from "./Asterisk.vue"
+import BuildingIcon from "./BuildingIcon.vue";
 
 export default {
   name: "AddressForm.vue",
   components: {
     Asterisk,
+    BuildingIcon,
     InputText,
     InputNumber,
     Button,
@@ -141,6 +131,9 @@ export default {
     settlementClasses() {
       return ["w-100", ...this.getClasses(this.settlement)]
     },
+    planStructureClasses() {
+      return ["w-100", ...this.getClasses(this.planStructure)]
+    },
     streetClasses() {
       return ["w-100", ...this.getClasses(this.street)]
     },
@@ -159,10 +152,11 @@ export default {
     validate() {
       this.region.valid = this.address.region.length > 0
       this.settlement.valid = this.address.settlement.length > 0
-      this.street.valid = this.address.street.length > 0
+      this.street.valid = this.address.street.length > 0 || this.address.planStructure.length > 0
+      this.planStructure.valid = this.address.street.length > 0 || this.address.planStructure.length > 0
       this.house.valid = this.address.house.length > 0
 
-      if (this.region.valid && this.settlement.valid && this.street.valid && this.house.valid) {
+      if (this.region.valid && this.settlement.valid && this.planStructure.valid && this.street.valid && this.house.valid) {
         this.$emit("valid")
       }
     },
