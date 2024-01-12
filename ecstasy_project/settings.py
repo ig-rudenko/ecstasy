6db68cbaf73476aa5d9bb2ad29942cd31200d640
ig-rudenko <ig.rudenko1@yandex.ru>
2024-01-12 09:45:41 +0300

    v 17.5.10 Параметр CSRF_TRUSTED_ORIGINS добавлен через переменные окружения

--- ecstasy_project/settings.py
+++ ecstasy_project/settings.py
@@ -42,6 +42,9 @@ DEBUG = os.getenv("DJANGO_DEBUG") == "1"
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1").split()
 
+if os.getenv("DJANGO_CSRF_TRUSTED_ORIGINS"):
+    CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS").split(",")
+
 # Application definition
 INSTALLED_APPS = [
     "jazzmin",
