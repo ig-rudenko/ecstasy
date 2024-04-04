b046b802d866faf1fe36dc140deb5379326e30b0
ig-rudenko <ig.rudenko1@yandex.ru>
2024-04-04 12:14:47 +0300

    v 17.7.26 Изменение админской панели Bugfix кнопки выхода

--- gathering/admin.py
+++ gathering/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
+
 from .models import MacAddress
 from .paginator import LargeTablePaginator
 
 
 @admin.register(MacAddress)
 class MacAddressesAdmin(admin.ModelAdmin):
-    list_display = ["address", "vlan", "device", "port", "datetime"]
+    list_display = ["address", "vlan", "device", "port", "desc", "datetime"]
+    search_fields = ["address", "vlan"]
     list_select_related = ["device"]
     paginator = LargeTablePaginator
