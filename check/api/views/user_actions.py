2721979bd18bb2540cfcafd981a276454b86cb5d
ig-rudenko <ig.rudenko1@yandex.ru>
2024-05-21 10:06:11 +0300

    v 17.10.6 Bugfix users actions api view

--- check/api/views/user_actions.py
+++ check/api/views/user_actions.py
@@ -1,13 +1,20 @@
+from rest_framework.mixins import ListModelMixin
+from rest_framework.response import Response
+
 from .base import DeviceAPIView
 from ..serializers import UserDeviceActionSerializer
 from ...models import UsersActions
 
 
-class UserDeviceActionsAPIView(DeviceAPIView):
+class UserDeviceActionsAPIView(DeviceAPIView, ListModelMixin):
     serializer_class = UserDeviceActionSerializer
 
-    def get_queryset(self):
+    def get(self, request, *args, **kwargs):
         device = self.get_object()
-        return UsersActions.objects.filter(device__name=device.name).values(
-            "time", "user", "action", "user__username"
-        )
+        queryset = (
+            UsersActions.objects.filter(device__name=device.name).values(
+                "time", "user", "action", "user__username"
+            )
+        )[:200]
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
