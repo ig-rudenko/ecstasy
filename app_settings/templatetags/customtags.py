2a1d7ca95f4a54f8947ded9b507d9054f151cfe4
ig-rudenko <ig.rudenko1@yandex.ru>
2024-09-03 23:18:18 +0300

    v 17.14.6 Обновление зависимостей и bugfix jazmin

---
+++ app_settings/templatetags/customtags.py
@@ -0,0 +1,12 @@
+from django.template.library import Library
+
+register = Library()
+
+
+@register.filter(is_safe=False)
+def length_is(value, arg):
+    """Return a boolean of whether the value's length is the argument."""
+    try:
+        return len(value) == int(arg)
+    except (ValueError, TypeError):
+        return ""
