210e580ac78456fe96f4db752a7d179b675d6b86
ig-rudenko <ig.rudenko1@yandex.ru>
2024-11-03 12:37:52 +0300

    v 18 Главная страница

--- frontend_v/src/main.ts
+++ frontend_v/src/main.ts
@@ -1,27 +1,22 @@
-import Avatar from "primevue/avatar";
-import Badge from "primevue/badge";
-import Button from "primevue/button";
-import ButtonGroup from "primevue/buttongroup";
-import Card from "primevue/card";
-import Column from "primevue/column";
-import DataTable from 'primevue/datatable';
-import Dialog from "primevue/dialog";
-import IconField from "primevue/iconfield";
-import InputNumber from "primevue/inputnumber";
-import Image from "primevue/image";
-import InputIcon from "primevue/inputicon";
-import InputText from "primevue/inputtext";
-import {IftaLabel, Menubar, Message, Password, Select} from "primevue";
-import Paginator from "primevue/paginator";
-import ProgressSpinner from "primevue/progressspinner";
-import Popover from "primevue/popover";
-import Drawer from "primevue/drawer";
-import Ripple from "primevue/ripple";
-import SelectButton from "primevue/selectbutton";
-import Splitter from "primevue/splitter";
-import SplitterPanel from "primevue/splitterpanel";
-import Timeline from "primevue/timeline";
-import Tooltip from 'primevue/tooltip';
+import {
+    Avatar,
+    Badge,
+    Button,
+    Column,
+    DataTable,
+    Dialog,
+    IftaLabel,
+    InputNumber,
+    InputText,
+    Menubar,
+    Message,
+    Paginator,
+    Password,
+    Popover,
+    Ripple,
+    Select,
+    Tooltip
+} from "primevue";
 import ToastService from "primevue/toastservice";
 
 import "@/assets/base.css";
@@ -42,28 +37,18 @@ app.use(router);
 app.component("Avatar", Avatar);
 app.component("Badge", Badge);
 app.component("Button", Button);
-app.component("ButtonGroup", ButtonGroup);
-app.component("Card", Card);
 app.component("Column", Column);
 app.component("DataTable", DataTable);
 app.component("Dialog", Dialog);
-app.component("Drawer", Drawer);
-app.component("IconField", IconField);
 app.component("IftaLabel", IftaLabel);
 app.component("InputNumber", InputNumber);
 app.component("Image", Image);
-app.component("InputIcon", InputIcon);
 app.component("InputText", InputText);
 app.component("Menubar", Menubar);
 app.component("Message", Message);
 app.component("Password", Password);
 app.component("Paginator", Paginator);
-app.component("ProgressSpinner", ProgressSpinner);
 app.component("Popover", Popover);
 app.component("Select", Select);
-app.component("SelectButton", SelectButton);
-app.component("Splitter", Splitter);
-app.component("SplitterPanel", SplitterPanel);
-app.component("Timeline", Timeline);
 
 app.mount('#app');
--- frontend_v/src/pages/home/Home.vue
+++ frontend_v/src/pages/home/Home.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
+import Welcome from "@/pages/home/Welcome.vue";
 </script>
 
 <template>
   <Header/>
+  <Welcome/>
   <Footer/>
 </template>
 
