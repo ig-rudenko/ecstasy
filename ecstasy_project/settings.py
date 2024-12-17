ab3394480a65fc33e7b8ccad73a7509c5f1bf838
Игорь Руденко <ig.rudenko1@yandex.ru>
2024-12-17 10:09:47 +0300

    v 18 Добавлена переменная окружения DJANGO_MEDIA_ROOT

--- ecstasy_project/settings.py
+++ ecstasy_project/settings.py
@@ -193,7 +193,8 @@ else:
     ]
 
 MEDIA_URL = "media/"
-MEDIA_ROOT = BASE_DIR / "media"
+
+MEDIA_ROOT = Path(os.getenv("DJANGO_MEDIA_ROOT", BASE_DIR / "media"))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
