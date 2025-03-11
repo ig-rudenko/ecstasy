e213ff4ffe5a9e77982f692c0564d64115b09738
Игорь Руденко <ig.rudenko1@yandex.ru>
2025-03-11 10:26:04 +0300

    v 18.3.1 Безопасно выполняет пользовательские команды с помощью блокировки сессии на оборудовании

--- devicemanager/remote/connector.py
+++ devicemanager/remote/connector.py
@@ -193,5 +193,5 @@ class RemoteDevice(
     ) -> MACTableType:
         return self._remote_call("vlans_on_port", port=port, operation=operation, vlans=vlans, tagged=tagged)
 
-    def execute_command(self, command: str) -> str:
-        return self._remote_call("execute_command", command=command)
+    def execute_command(self, cmd: str) -> str:
+        return self._remote_call("execute_command", cmd=cmd)
