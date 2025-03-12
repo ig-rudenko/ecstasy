989defb9dd059b30e6c99c412442403e796afc25
Игорь Руденко <ig.rudenko1@yandex.ru>
2025-03-12 10:50:05 +0300

    v 18.3.2 Можно создавать несколько API токенов для одного пользователя

--- accounting/admin.py
+++ accounting/admin.py
@@ -1,26 +1,7 @@
 from django.contrib import admin
-from django.contrib.admin.utils import quote
-from django.contrib.admin.views.main import ChangeList
-from django.contrib.auth import get_user_model
-from django.core.exceptions import ValidationError
-from django.urls import reverse
 from django.utils import timezone
 
 from .models import UserAPIToken
-
-User = get_user_model()
-
-
-class TokenChangeList(ChangeList):
-    """Map to matching User id"""
-
-    def url_for_result(self, result):
-        pk = result.user.pk
-        return reverse(
-            "admin:%s_%s_change" % (self.opts.app_label, self.opts.model_name),
-            args=(quote(pk),),
-            current_app=self.model_admin.admin_site.name,
-        )
 
 
 @admin.register(UserAPIToken)
@@ -29,7 +10,6 @@ class CustomTokenAdmin(admin.ModelAdmin):
     search_fields = ("user__username", "description")
     readonly_fields = ("key", "last_used")
     ordering = ("-created",)
-    actions = None  # Actions not compatible with mapped IDs.
 
     fieldsets = (
         (
@@ -46,24 +26,3 @@ class CustomTokenAdmin(admin.ModelAdmin):
     def verbose_info(self, obj: UserAPIToken):
         status = "❌" if obj.expired and obj.expired < timezone.now() else "✅"
         return f"{status} {obj.user}"
-
-    def get_changelist(self, request, **kwargs):
-        return TokenChangeList
-
-    def get_object(self, request, object_id, from_field=None):
-        """
-        Map from User ID to matching Token.
-        """
-        queryset = self.get_queryset(request)
-        field = User._meta.pk
-        try:
-            object_id = field.to_python(object_id)
-            user = User.objects.get(**{field.name: object_id})
-            return queryset.get(user=user)
-        except (queryset.model.DoesNotExist, User.DoesNotExist, ValidationError, ValueError):
-            return None
-
-    def delete_model(self, request, obj):
-        # Map back to actual Token, since delete() uses pk.
-        token = UserAPIToken.objects.get(key=obj.key)
-        return super().delete_model(request, token)
