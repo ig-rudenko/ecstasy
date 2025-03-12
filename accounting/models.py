409d2b05d4ae06dde567a97a8120fcf5343b52b4
Игорь Руденко <ig.rudenko1@yandex.ru>
2025-03-12 10:44:28 +0300

    v 18.3.2 Можно создавать несколько API токенов для одного пользователя

--- accounting/models.py
+++ accounting/models.py
@@ -1,8 +1,15 @@
+from django.conf import settings
 from django.db import models
 from rest_framework.authtoken.models import Token
 
 
 class UserAPIToken(Token):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="auth_token",
+        on_delete=models.CASCADE,
+        verbose_name="Пользователь",
+    )
     description = models.TextField(help_text="Напишите для каких целей будет использоваться токен")
     expired = models.DateTimeField(
         null=True, blank=True, help_text="Укажите время истечения токена, если нужно"
