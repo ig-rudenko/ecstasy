54f4e571c2a4a47a173f2f77b1b88e8198018ab3
Игорь Руденко <ig.rudenko1@yandex.ru>
2025-07-30 09:06:26 +0300

    v 18.4.7 update JWT lifetime

--- ecstasy_project/settings.py
+++ ecstasy_project/settings.py
@@ -347,8 +347,8 @@ if not DEBUG:
 # ================= JWT ===================
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=15),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": True,
