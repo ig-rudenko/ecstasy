db60510c68273ca3d9d302c1351698c08a27bac5
Игорь Руденко <ig.rudenko1@yandex.ru>
2026-03-19 17:05:29 +0300

    Bugfix gpon CustomerView connected_at field

--- frontend_v/src/formats.ts
+++ frontend_v/src/formats.ts
@@ -6,6 +6,12 @@ export function verboseDatetime(date: string): string {
     return dateObject.toLocaleString("ru-RU",)
 }
 
+export function verboseDate(date: string): string {
+    // multiplied by 1000 so that the argument is in milliseconds, not seconds
+    const dateObject = new Date(date);
+    return dateObject.toLocaleString("ru-RU", {year: "numeric", month: "2-digit", day: "2-digit"})
+}
+
 export function getAvatar(username: string, image?: string, size: number = 64) {
     if (image) return image;
     return `https://ui-avatars.com/api/?size=${size}&name=${username}&font-size=0.33&background=random&rounded=true`
