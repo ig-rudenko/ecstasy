87319ce50db78b2a22178c5158a9635d858265f1
Игорь Руденко <ig.rudenko1@yandex.ru>
2026-05-21 12:02:23 +0300

    v 20.0.0 - Bugfix test - update CI

--- apps/ring_manager/tests/base.py
+++ apps/ring_manager/tests/base.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 from apps.check.models import AuthGroup, DeviceGroup, Devices
 from apps.net_tools.models import DevicesInfo
 
-from ..models import RingDev
+from ..models import RingDev, TransportRing
 
 
 class TestRingBase(TransactionTestCase):
@@ -63,12 +63,9 @@ class TestRingBase(TransactionTestCase):
                 last_ring_dev = first_ring_dev
 
         # Создаем кольцо
-        # print(
-        #     "Создаем кольцо",
-        #     TransportRing.objects.create(
-        #         name=self.ring_name,
-        #         head=first_ring_dev,
-        #         tail=last_ring_dev,
-        #         vlans=self.RING_VLANS,
-        #     ),
-        # )
+        TransportRing.objects.create(
+            name=self.ring_name,
+            head=first_ring_dev,
+            tail=last_ring_dev,
+            vlans=self.RING_VLANS,
+        ),
